cd1e53477c981fb34debf280ba6a58db1d864bb9
jre9158 <80225531+jre9158@users.noreply.github.com>
2024-06-27 22:41:59 -0400

    re organizing the test cases.
    re organizing the test cases.

--- testers/JottParserTester.java
+++ testers/JottParserTester.java
@@ -83,7 +83,6 @@ public class JottParserTester {
 
     public void AddPhase3TestCases()
     {
-
         //Working in last batch test
         testCases.add(new TestCase("funcCallParamInvalid", "funcCallParamInvalid.jott", true ));
         testCases.add(new TestCase("funcNotDefined", "funcNotDefined.jott", true ));
@@ -101,11 +100,11 @@ public class JottParserTester {
         testCases.add(new TestCase("whileKeyword", "whileKeyword.jott", true ));
         testCases.add(new TestCase("helloWorld", "helloWorld.jott", false ));
         testCases.add(new TestCase("providedExample1", "providedExample1.jott", false ));
-
-        //ISSUES WITH BELOW
         testCases.add(new TestCase("ifStmtReturns", "ifStmtReturns.jott", false ));
-        //testCases.add(new TestCase("largerValid", "largerValid.jott", false ));
         testCases.add(new TestCase("validLoop", "validLoop.jott", false ));
+
+        //Still broken below
+        testCases.add(new TestCase("largerValid", "largerValid.jott", false ));
 
     }
 
